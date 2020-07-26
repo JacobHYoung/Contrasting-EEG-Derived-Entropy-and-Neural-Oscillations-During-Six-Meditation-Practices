29a12058995b31556530f4b3b6fe2241d2391895
Ross Kempner <rkempner@umich.edu>
2020-07-25 22:08:34 -0400

    testing different ways to compute app en

--- code/python/entropy_analysis/entropy_EEG.py
+++ code/python/entropy_analysis/entropy_EEG.py
@@ -10,6 +10,35 @@ Plan: Use at least three different python libraries calculation of approximate e
 # import various libraries  
 import mne_features
 import mne 
+import entropy
 import numpy as np
+import matplotlib 
+import matplotlib.pyplot as plt
 
-# lets create some fake data to test the various functions. Mne_features has a function for computing the approximate entropy that 
+
+# lets create some fake data to test the various functions
+fake_data = np.random.rand(10,1000)
+
+# testing mne_features
+print("approximate_entropy with mne_features: ",mne_features.univariate.compute_app_entropy(fake_data, emb=2, metric='chebyshev'))
+mne_ap_en = mne_features.univariate.compute_app_entropy(fake_data, emb=2, metric='chebyshev')
+# create a graph with the channels as the x column and the entropy as the y column
+fig, ax = plt.subplots()
+channel_list = []
+for index, channel_row in enumerate(fake_data):
+    channel_list.append(index)
+ax.plot(channel_list,mne_ap_en)
+
+ax.set(xlabel='Channels', ylabel='ApEn',
+       title='Approximate Entropy of 10 Fake Data Channels Over 1000 Fake Time Points: ')
+ax.grid()
+
+fig.savefig("mne_features_ap_en_fake_data.png")
+plt.show()
+
+# testing EntroPy
+EntroPy_app_entropy_list = []
+for channel_row in fake_data:
+    new_channel_entropy = entropy.app_entropy(channel_row, order=2, metric='chebyshev')
+    EntroPy_app_entropy_list.append(new_channel_entropy)
+print("approximate_entropy with EntroPy: ", EntroPy_app_entropy_list)
